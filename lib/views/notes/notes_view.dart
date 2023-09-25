0aafe2c3e056b97c4560bcdf7697c353f3501dfa
Meekam Okeke <meekam.okeke@accenture.com>
2023-09-25 15:59:42 -0500

    step-14

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -37,7 +37,7 @@ class _NotesViewState extends State<NotesView> {
           IconButton(onPressed: () {
             Navigator.of(context).pushNamed(newNoteRoute);
           }, 
-          icon: const Icon(Icons.add)
+          icon: const Icon(Icons.add),
           ),
           PopupMenuButton<MenuAction>(
             onSelected: (value) async {
@@ -74,6 +74,7 @@ class _NotesViewState extends State<NotesView> {
                   builder: (context, snapshot) {
                     switch (snapshot.connectionState) {
                       case ConnectionState.waiting:
+                      case ConnectionState.active:
                        return const Text('Waiting for all notes...');
                       default:
                         return const CircularProgressIndicator();
