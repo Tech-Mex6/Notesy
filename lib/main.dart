ff49949e3bbe2bca8bb286670b8c1997617043ed
Meekam Okeke <meekam.okeke@accenture.com>
2023-09-25 13:00:27 -0500

    step-13

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:notesy/constants/routes.dart';
 import 'package:notesy/services/auth/auth_service.dart';
 import 'package:notesy/views/login_view.dart';
-import 'package:notesy/views/notes_view.dart';
+import 'package:notesy/views/notes/new_note_view.dart';
+import 'package:notesy/views/notes/notes_view.dart';
 import 'package:notesy/views/register_view.dart';
 import 'package:notesy/views/verify_email_view.dart';
 
@@ -22,6 +23,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
+        newNoteRoute:(context) => const NewNoteView(),
       },
     ),
   );
