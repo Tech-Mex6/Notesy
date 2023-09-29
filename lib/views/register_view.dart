9a3e25f2416c2896a942af4ccba82d6dfde199f7
Meekam Okeke <meekam.okeke@accenture.com>
2023-09-29 14:06:19 -0500

    step-16

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:notesy/constants/routes.dart';
 import 'package:notesy/services/auth/auth_exceptions.dart';
 import 'package:notesy/services/auth/auth_service.dart';
-import 'package:notesy/utilities/show_error_dialog.dart';
+import 'package:notesy/utilities/dialogs/error_dialog.dart';
 
 
 class RegisterView extends StatefulWidget {
