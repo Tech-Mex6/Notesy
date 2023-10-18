bac7217e49a2c516ac6d9d6f6ef6776dee88a3f1
Meekam Okeke <meekam.okeke@accenture.com>
2023-10-18 11:43:51 -0500

    step-21

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -4,6 +4,9 @@ import 'package:notesy/services/auth/auth_service.dart';
 import 'package:notesy/utilities/generics/get_arguments.dart';
 import 'package:notesy/services/cloud/cloud_note.dart';
 import 'package:notesy/services/cloud/firebase_cloud_storage.dart';
+import 'package:share_plus/share_plus.dart';
+
+import '../../utilities/dialogs/cannot_share_empty_note_dialog.dart';
 
 
 
@@ -90,6 +93,18 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('New Note'),
+        actions: [
+          IconButton(onPressed: () async {
+            final text = _textController.text;
+            if (_note != null || text.isEmpty) {
+              await showCannotShareEmptyNoteDialog(context);
+            } else {
+              Share.share(text);
+            }
+          }, 
+          icon: const Icon(Icons.share),
+         ),
+        ],
       ),
       body: FutureBuilder(
         future: createOrGetExistingNote(context),
